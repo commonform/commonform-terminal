63eaa07704085ae708b739474d7a3a6fd51f7707
Kyle Mitchell <kyle@kemitchell.com>
2015-02-07 08:00:45 +0000

    more informative display

--- source/templates/run.js
+++ source/templates/run.js
@@ -1,19 +1,17 @@
 var chalk = require('chalk');
 
-var BLANK = '__________';
-
 module.exports = function run(element, numberStyle, conspicuous) {
   if (typeof element === 'string') {
     return conspicuous ? chalk.magenta(element) : element;
   } else if (element.hasOwnProperty('definition')) {
     return '"' + chalk.green(element.definition) + '"';
   } else if (element.hasOwnProperty('blank')) {
-    return chalk.red(BLANK);
+    return chalk.red.underline(element.blank);
   } else if (element.hasOwnProperty('reference')) {
     if (element.broken || element.ambiguous) {
       return chalk.yellow(element.reference);
     } else {
-      return chalk.yellow(numberStyle.reference(element.reference));
+      return chalk.cyan(numberStyle.reference(element.reference));
     }
   } else {
     throw new Error('Invalid type: ' + JSON.stringify(element));
