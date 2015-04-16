04f723347fdb5718eb6f474a178a6559a6cd1f67
Kyle Mitchell <kyle@kemitchell.com>
2015-04-16 08:38:34 +0000

    chomp newlines off the ends of output strings

--- source/index.js
+++ source/index.js
@@ -1,3 +1,7 @@
-module.exports = require('./templates/document');
+var doc = require('./templates/document');
+module.exports = function() {
+  var codes = doc.apply(this, arguments);
+  return codes.slice(0, codes.length - 2);
+};
 
 module.exports.version = '0.4.1';
