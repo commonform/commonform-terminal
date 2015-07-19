e0c355316d1fbe7a1003964aae2fa5838c8cf7a8
Kyle Mitchell <kyle@kemitchell.com>
2015-07-19 12:57:48 -0700

    Include form heading in rendered references

--- source/templates/run.js
+++ source/templates/run.js
@@ -7,15 +7,19 @@ module.exports = function run(element, numberStyle, conspicuous) {
     return '"' + chalk.green(element.definition) + '"';
   } else if (element.hasOwnProperty('blank')) {
     return chalk.red.underline(element.blank);
-  } else if (element.hasOwnProperty('reference')) {
-    var reference = element.reference;
+  } else if (element.hasOwnProperty('numbering')) {
+    var numbering = element.numbering;
+    var heading = element.heading;
     if (
       element.hasOwnProperty('broken') ||
       element.hasOwnProperty('ambiguous')
     ) {
-      return chalk.yellow(reference);
+      return chalk.yellow(heading);
     } else {
-      return chalk.cyan('Section ' + numberStyle(reference));
+      return chalk.cyan(
+        'Section ' + numberStyle(numbering) +
+        ' (' + heading + ')'
+      );
     }
   } else {
     throw new Error('Invalid type: ' + JSON.stringify(element));
