b9f3695cd234b747eda7a7516c225f44f6e9e5bb
Ansel Halliburton <github@anseljh.com>
2015-09-02 15:22:44 -0700

    Use a correct color for `run`

--- run.js
+++ run.js
@@ -20,7 +20,7 @@ module.exports = function run(element, numberStyle, conspicuous) {
   } else if (element.hasOwnProperty('definition')) {
     return '"' + chalk.green(element.definition) + '"';
   } else if (element.hasOwnProperty('use')) {
-    return '"' + chalk.orange(element.definition) + '"';
+    return '"' + chalk.magenta(element.definition) + '"';
   } else if (element.hasOwnProperty('blank')) {
     return chalk.red.underline(element.blank);
   } else if (element.hasOwnProperty('heading')) {
