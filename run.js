839728ca79babf353b8ffd3cf64cd27ea24ff2c3
Kyle Mitchell <kyle@kemitchell.com>
2015-11-01 15:29:24 -0800

    Upgrade to commonform-flatten@0.8.x

--- run.js
+++ run.js
@@ -22,7 +22,7 @@ module.exports = function run(element, numberStyle, conspicuous) {
   } else if (element.hasOwnProperty('use')) {
     return chalk.magenta(element.use);
   } else if (element.hasOwnProperty('blank')) {
-    return chalk.red.underline('value' in element ? element.value : element.blank);
+    return chalk.red.underline('[•]');
   } else if (element.hasOwnProperty('heading')) {
     var numbering = element.numbering;
     var heading = element.heading;
