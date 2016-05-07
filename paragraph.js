0849db0a5e19d006e15bad7785270f1f89254f55
Kyle E. Mitchell <kyle@kemitchell.com>
2016-05-06 23:35:31 -0700

    Display word objects in headings

--- paragraph.js
+++ paragraph.js
@@ -21,13 +21,13 @@ module.exports = function(paragraph, numberStyle) {
   var number = paragraph.hasOwnProperty('numbering') ?
     paragraph.numbering : '';
   var conspicuous = paragraph.hasOwnProperty('conspicuous');
+  function runify(element) {
+    return run(element, numberStyle, conspicuous) }
   return new Array(paragraph.depth * 4 + 1).join(' ') +
     (number ?
       chalk.gray(numberStyle(number)) + TAB : '') +
     (paragraph.hasOwnProperty('heading') ?
-      chalk.blue.underline(paragraph.heading) + '. ' : '') +
-    paragraph.content.map(function(element) {
-      return run(element, numberStyle, conspicuous);
-    }).join('') +
+      chalk.blue.underline(paragraph.heading.map(runify)).join('') + '. ' : '') +
+    paragraph.content.map(runify).join('') +
     '\n\n';
 };
