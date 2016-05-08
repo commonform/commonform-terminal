6e9d3b88ead9872864760eb1a8149b2022120034
Kyle E. Mitchell <kyle@kemitchell.com>
2016-05-07 21:39:34 -0700

    Reimplement

--- index.js
+++ index.js
@@ -12,9 +12,62 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-var doc = require('./document');
+module.exports = stringify
 
-module.exports = function() {
-  var codes = doc.apply(this, arguments);
-  return codes.slice(0, codes.length - 2);
-};
+var ansi = require('ansi-styles')
+
+var styles = {
+  word: id,
+  use: wrap(ansi.blue),
+  definition: wrap(ansi.green),
+  reference: wrap(ansi.red),
+  blank: id }
+
+function stringify(diff) {
+  return childString({ heading: [ ], form: diff }, 0) }
+
+function contentString(content) {
+  var returned = ''
+
+  var wrapper = id
+  if (content.hasOwnProperty('deleted')) {
+    wrapper = wrap(ansi.strikethrough) }
+  else if (content.hasOwnProperty('inserted')) {
+    wrapper = wrap(ansi.underline) }
+
+  Object.keys(styles)
+    .forEach(function(key) {
+      if (content.hasOwnProperty(key)) {
+        returned += styles[key](
+          ( key === 'blank'
+              ? '[________]'
+              : content[key] )) } })
+
+  return wrapper(returned) }
+
+var INDENT = '    '
+
+function childString(child, depth) {
+  var returned = INDENT.repeat(depth)
+  if (child.heading.length !== 0) {
+    returned += wrap(ansi.bold)(
+      child.heading
+        .map(function(element) {
+          return contentString(element) })
+        .join('') +
+      ' ') }
+  returned += child.form.content
+    .map(function(element) {
+      if (element.hasOwnProperty('form')) {
+        return ( '\n\n' + childString(element, ( depth + 1 )) ) }
+      else {
+        return contentString(element, process.stdout) } })
+      .join('')
+  return returned }
+
+function id(x) {
+  return x }
+
+function wrap(style) {
+  return function(argument) {
+    return ( style.open + argument + style.close ) } }
